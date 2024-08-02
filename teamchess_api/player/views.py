a28f7b841781bcd16cd9b0e9035b1ed17d73780c
Saruar Star <saruar.star1999@gmail.com>
2024-08-02 17:12:35 +0300

    optimize player views

--- teamchess_api/player/views.py
+++ teamchess_api/player/views.py
@@ -8,42 +8,26 @@ from .permissions import CanManipulatePlayer
 from . import serializers
 
 
-class KickPlayerView(CreateAPIView):
+class BaseManipulatePlayerView(CreateAPIView):
+    queryset = Player.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated, CanManipulatePlayer]
+
+    def create(self, request, *args, **kwargs):
+        player = self.get_object()
+        serializer = self.get_serializer(player, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(status=status.HTTP_200_OK)
+
+
+class KickPlayerView(BaseManipulatePlayerView):
     serializer_class = serializers.KickPlayerSerializer
-    queryset = Player.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated, CanManipulatePlayer]
-    
-    def create(self, request, *args, **kwargs):
-        player = self.get_object()
-        serializer = self.get_serializer(player, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        return Response(status=status.HTTP_200_OK)
 
-class ChangePlayerTeamView(CreateAPIView):
+
+class ChangePlayerTeamView(BaseManipulatePlayerView):
     serializer_class = serializers.ChangePlayerColorSerializer
-    queryset = Player.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated, CanManipulatePlayer]
-    
-    def create(self, request, *args, **kwargs):
-        player = self.get_object()
-        serializer = self.get_serializer(player, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        return Response(status=status.HTTP_200_OK)
     
 
-class SwapPlayerView(CreateAPIView):
+class SwapPlayerView(BaseManipulatePlayerView):
     serializer_class = serializers.SwapPlayerSerializer
-    queryset = Player.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated, CanManipulatePlayer]
-    
-    def create(self, request, *args, **kwargs):
-        player = self.get_object()
-        serializer = self.get_serializer(player, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        return Response(status=status.HTTP_200_OK)
